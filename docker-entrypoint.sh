3ecc7b8c187a2bc3ee15bfc6b2d925aebca2016d
KagurazakaNyaa <dev@kagurazakanyaa.com>
2022-12-15 15:32:04 +0800

    混合数据集训练参数

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -5,7 +5,11 @@ if [ -z "$(ls -A /workspace/synthesizer/saved_models)" ] || [ "$FORCE_RETRAIN" =
     /workspace/datasets_download/extract.sh
     for DATASET in ${TRAIN_DATASETS}
     do
-        python pre.py /datasets -d ${DATASET} -n $(nproc)
+        if [ "$TRAIN_SKIP_EXISTING" = true ] ; then
+            python pre.py /datasets -d ${DATASET} -n $(nproc) --skip_existing
+        else
+            python pre.py /datasets -d ${DATASET} -n $(nproc)
+        fi
     done
     python synthesizer_train.py mandarin /datasets/SV2TTS/synthesizer
 fi
